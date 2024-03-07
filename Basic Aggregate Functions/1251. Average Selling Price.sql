5df4afe90c23d1fad062b0638e0a4eb41a4b8e79
Jayanth-BS <109243649+Jayanth-BS@users.noreply.github.com>
2024-03-07 23:58:00 +0530

    Create 1251. Average Selling Price.sql

---
+++ Basic Aggregate Functions/1251. Average Selling Price.sql
@@ -0,0 +1,43 @@
+-- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Prices table:
+-- +------------+------------+------------+--------+
+-- | product_id | start_date | end_date   | price  |
+-- +------------+------------+------------+--------+
+-- | 1          | 2019-02-17 | 2019-02-28 | 5      |
+-- | 1          | 2019-03-01 | 2019-03-22 | 20     |
+-- | 2          | 2019-02-01 | 2019-02-20 | 15     |
+-- | 2          | 2019-02-21 | 2019-03-31 | 30     |
+-- +------------+------------+------------+--------+
+-- UnitsSold table:
+-- +------------+---------------+-------+
+-- | product_id | purchase_date | units |
+-- +------------+---------------+-------+
+-- | 1          | 2019-02-25    | 100   |
+-- | 1          | 2019-03-01    | 15    |
+-- | 2          | 2019-02-10    | 200   |
+-- | 2          | 2019-03-22    | 30    |
+-- +------------+---------------+-------+
+-- Output: 
+-- +------------+---------------+
+-- | product_id | average_price |
+-- +------------+---------------+
+-- | 1          | 6.96          |
+-- | 2          | 16.96         |
+-- +------------+---------------+
+
+
+-- Solution:
+SELECT p.product_id, ifnull(round(sum(p.price * s.units)/sum(s.units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold s ON p.product_id = s.product_id
+AND s.purchase_date >= p.start_date AND s.purchase_date <= p.end_date
+GROUP BY p.product_id 
